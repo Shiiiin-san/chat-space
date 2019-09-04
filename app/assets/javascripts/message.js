1f031e5c5ff1cd3c2f8c24214c0bb4f1064f0c4e
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-04 18:50:03 +0900

    ajaxを設定、messages#createに送信機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,22 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    console.log(this);
-  })
-})
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  console.log(this)
+  var url = $(this).attr('action');
+  $.ajax({
+      type: "POST",
+      url:  url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      // 成功の場合の処理
+    })
+    .fail({
+      // エラーの場合処理
+    });
+  });
+});
