58462eb746c4f0142e897fc8e8300867cdc57a84
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-06 17:38:55 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var img = message.image ? `<img class = "main-manu__one-message-detail--image" src="${message.image}">` : '';
-    var html = `<div class = "main-manu__one-message">
+    var html = `<div class = "main-manu__one-message" data-id="${message.id}">
                   <ul class = "main-manu__one-message-user">
                     <li class = "main-manu__one-message-name">
                       ${message.name}
@@ -50,4 +50,27 @@ $('#new_message').on('submit', function(e){
       $('.main-manu__submission-form-submit').removeAttr('disabled')
     });
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $(".main-manu__one-message").data("one_message")
+
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "/api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      messages.forEach(function(message){
+        var insertHTML = buildHTML(message)
+        $('.main-manu__message').append(insertHTML)
+      })
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
