d00928b1878cd9e796d84a1ef4ef825454b714b7
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-11 14:04:54 +0900

    git修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.11.1'
 set :application, 'shat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/Shiiiin-san/chat-space.git'
+set :repo_url,  'git@github.com:Shiiiin-san/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
