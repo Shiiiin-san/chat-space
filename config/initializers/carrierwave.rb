3990f6f684105d7d8bf17296c19a156c41051fba
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-12 13:13:09 +0900

    スペルミス修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space-image-uploder'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-spase-image-uploder'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-image-uploder'
 end
