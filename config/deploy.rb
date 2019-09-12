032e35a635d46869d0bf6bb3c1487c141b2e6b37
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-12 12:46:11 +0900

    set :default_envの位置変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,14 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.1'
 
+# 環境変数をcapistranoでの自動デプロイで利用するため、環境変数を明示的に指定
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -46,11 +54,3 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
-
-# 環境変数をcapistranoでの自動デプロイで利用するため、環境変数を明示的に指定
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
